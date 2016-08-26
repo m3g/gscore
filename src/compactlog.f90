5ccec878f1cda86921d4b314cb4d5606731a9708
Leandro Martinez <leandromartinez98@gmail.com>
2016-08-26 08:25:29 -0300

    Added safeguards in model_index and added Makefile

--- src/compactlog.f90
+++ src/compactlog.f90
@@ -14,16 +14,6 @@
 ! http://leandro.iqm.unicamp.br
 !
 
-module types
-
-  type model_type
-    character(len=200) :: name
-    double precision :: pas_gdt
-    double precision :: pas_tm
-  end type model_type
-
-end module types
-
 program compactlog
 
   use types
@@ -34,7 +24,7 @@ program compactlog
   double precision, allocatable :: gdt(:,:), tmscore(:,:)
   character(len=200) :: align_list, firstlog, align_log, gdt_log, tm_log
   character(len=200) :: basename, record, file1, file2, format
-  logical :: comment
+  logical :: comment, error
   type(model_type), allocatable :: model(:)
 
   narg = iargc()
@@ -199,9 +189,11 @@ program compactlog
         stop
       end if
       file1 = basename(file1)
-      i1 = model_index(file1,model,nmodels)
+      i1 = model_index(file1,model,nmodels,error)
+      if ( error ) cycle
       file2 = basename(file2)
-      i2 = model_index(file2,model,nmodels)
+      i2 = model_index(file2,model,nmodels,error)
+      if ( error ) cycle
       if ( i1 > i2 ) then
         tmscore(i1,i2) = tmscore_read
         gdt(i1,i2) = gdt_read
@@ -237,29 +229,43 @@ end program compactlog
 ! Function that determines the index of a model from its name
 !
 
-function model_index(name,model,n)
+function model_index(name,model,n,error)
  
   use types
   implicit none
   integer :: model_index, n, imax, imin, iavg
   character(len=200) :: name
+  logical :: error
   type(model_type) :: model(n)
 
   imin = 1
   imax = n
-  do
-    if ( name == model(imin)%name ) then 
-      model_index = imin
-      return
-    end if
-    if ( name == model(imax)%name ) then
-      model_index = imax
-      return
-    end if
-    iavg = imin + ( imax - imin ) / 2
-    if ( name >= model(iavg)%name ) imin = iavg
-    if ( name <= model(iavg)%name ) imax = iavg
-  end do
+  error = .false.
+  if ( name < model(1)%name ) error = .true.
+  if ( name > model(n)%name ) error = .true.
+  if ( .not. error ) then
+    do
+      if ( name == model(imin)%name ) then 
+        model_index = imin
+        return
+      end if
+      if ( name == model(imax)%name ) then
+        model_index = imax
+        return
+      end if
+      if ( imax == imin ) then
+        error = .true.
+        exit
+      end if
+      iavg = imin + ( imax - imin ) / 2
+      if ( name >= model(iavg)%name ) imin = iavg
+      if ( name <= model(iavg)%name ) imax = iavg
+    end do
+  end if
+  if ( error ) then
+    write(*,*) ' WARNING: A file is listed in a log file but was not found in list: '
+    write(*,*) '          File: ', trim(adjustl(name))
+  end if
 
 end function model_index
 
---
+++ src/types.f90
@@ -0,0 +1,9 @@
+module types
+
+  type model_type
+    character(len=200) :: name
+    double precision :: pas_gdt
+    double precision :: pas_tm
+  end type model_type
+
+end module types
