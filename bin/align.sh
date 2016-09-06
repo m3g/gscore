6fc626f6d73ea468df7f38700e5876084ba14300
Leandro Martinez <leandromartinez98@gmail.com>
2016-09-06 16:49:03 -0300

    Fixed align.sh, and added -nglobal 1 option.

--- bin/align.sh
+++ bin/align.sh
@@ -12,5 +12,5 @@ for model in `cat $pdblist`; do
   log=$logdir/$name.log
   wait_for.tcl $username lovoalign $nprocs 0.1
   echo "Running $name"
-  lovoalign -pdblist $pdblist -seqfix -skip > $log &
+  lovoalign -p1 $model -pdblist $pdblist -seqfix -skip -nglobal 1 > $log &
 done
