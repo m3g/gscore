c69c133b61c3605ee910ed3697463918afeaa9c1
Leandro Martinez <leandromartinez98@gmail.com>
2016-09-06 16:30:45 -0300

    Fixed align.sh script.

--- bin/align.sh
+++ bin/align.sh
@@ -6,10 +6,11 @@ nprocs=$3     # Number of processors of your machine
 # No modifications required after this
 #
 username=`whoami`
-for file in `cat $pdblist`; do
+for model in `cat $pdblist`; do
   name=$(basename $model)
   name=${name%.*}
   log=$logdir/$name.log
-  wait_for.tcl $whoami lovoalign $nprocs
+  wait_for.tcl $username lovoalign $nprocs 0.1
+  echo "Running $name"
   lovoalign -pdblist $pdblist -seqfix -skip > $log &
 done
